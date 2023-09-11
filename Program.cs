8430ba5ea9ac702d9ffaaba6252e9882b0c2ce47
varshangowda <97723895+varshangowda@users.noreply.github.com>
2023-09-11 09:39:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ass3.c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a first word ");
+            string name1 = Console.ReadLine();
+            Console.WriteLine("Enter a second word ");
+            string name2 = Console.ReadLine();
+
+            if(name1==name2)
+            {
+                Console.WriteLine("both words are same");
+            }
+            else
+            {
+                Console.WriteLine("both words are different");
+            }
+            Console.ReadLine();
+        }
+    }
+}
