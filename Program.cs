56b4bde00ad5309edf87f999cb5c7d4b50608443
varshangowda <97723895+varshangowda@users.noreply.github.com>
2023-09-11 10:04:35 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,25 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ass3.c
+namespace ass3.b
 {
-    class Program
+    class Reverse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a first word ");
-            string name1 = Console.ReadLine();
-            Console.WriteLine("Enter a second word ");
-            string name2 = Console.ReadLine();
-
-            if(name1==name2)
-            {
-                Console.WriteLine("both words are same");
-            }
-            else
-            {
-                Console.WriteLine("both words are different");
-            }
+            Console.WriteLine("ENTER A WORD");
+            string str = Console.ReadLine();
+            char[] stringArray = str.ToCharArray();
+            Array.Reverse(stringArray);
+            string reversedStr = new string(stringArray);
+            Console.WriteLine($"Actual string {str}\n");
+            Console.WriteLine($"Reversed string {reversedStr} ");
             Console.ReadLine();
         }
     }
